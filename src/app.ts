ad1e12214b18e6fd47a02a1e67ef4b6d9a40f414
tiagoruivo10 <tiagosantarim2017@gmail.com>
2026-04-20 17:38:07 -0300

    feat: adiciona validação de variáveis de ambiente com zod

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import type { FastifyInstance } from "fastify";
 import Fastify from "fastify";
+import { env } from "./config/env.js";
 import routes from "./routes/index.js";
 
 const app: FastifyInstance = Fastify({
-  logger: true,
+  logger: {
+    level: env.NODE_ENV === "dev" ? "info" : "error",
+  },
 });
 
 app.register(routes, { prefix: "/api" });
