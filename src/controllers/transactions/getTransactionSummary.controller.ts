819167d9cbaf03b18adaa51830b4339a2bb4b473
tiagoruivo10 <tiagosantarim2017@gmail.com>
2026-04-21 16:32:59 -0300

    terminando o resumo das transações.

--- src/controllers/transactions/getTransactionSummary.controller.ts
+++ src/controllers/transactions/getTransactionSummary.controller.ts
@@ -1,8 +1,11 @@
+import { TransactionType } from "@prisma/client";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc.js";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import prisma from "../../config/prisma.js";
 import type { GetTransactionsSummaryQuery } from "../../schemas/transaction.schema.js";
+import type { CategorySummary } from "../../types/category.types.js";
+import type { TransactionSummary } from "../../types/transaction.type.js";
 
 dayjs.extend(utc);
 
@@ -41,7 +44,42 @@ export const getTransactionSummary = async (
       },
     });
 
-    reply.send(transactions);
+    let totalExpenses = 0;
+    let totalIncomes = 0;
+    const groupedExpenses = new Map<string, CategorySummary>();
+
+    for (const transaction of transactions) {
+      if (transaction.type === TransactionType.expense) {
+        const existing = groupedExpenses.get(transaction.categoryId) ?? {
+          categoryId: transaction.categoryId,
+          categoryName: transaction.category.name,
+          categoryColor: transaction.category.color,
+          amount: 0,
+          percentage: 0,
+        };
+
+        existing.amount += transaction.amount;
+        groupedExpenses.set(transaction.categoryId, existing);
+
+        totalExpenses += transaction.amount;
+      } else {
+        totalIncomes += transaction.amount;
+      }
+    }
+
+    const summary: TransactionSummary = {
+      totalExpenses,
+      totalIncomes,
+      balance: totalIncomes - totalExpenses,
+      expensesByCategory: Array.from(groupedExpenses.values())
+        .map((entry) => ({
+          ...entry,
+          percentage: Number.parseFloat(((entry.amount / totalExpenses) * 100).toFixed(2)),
+        }))
+        .sort((a, b) => b.amount - a.amount),
+    };
+
+    reply.send(summary);
   } catch (err) {
     request.log.error(err as Error, "Erro ao trazer transações");
     reply.status(500).send({ error: "Erro interno do servidor" });
